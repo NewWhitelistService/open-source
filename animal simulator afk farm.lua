93d80c8e0396a31e066aebfd9f08606af194525d
NewWhitelistService <doitenroiugphone@gmail.com>
2024-11-30 02:30:17 +0700

    Update animal simulator afk farm.lua

--- animal simulator afk farm.lua
+++ animal simulator afk farm.lua
@@ -31,8 +31,8 @@ task.spawn(function()
             local Http = game:GetService("HttpService")
             local TPS = game:GetService("TeleportService")
             local Api = "https://games.roblox.com/v1/games/"
-            local _place, _id = game.PlaceId, game.JobId
-            local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=2"
+            local _place = game.PlaceId
+            local _servers = Api.._place.."/servers/Public"
 
             function ListServers(cursor)
                 local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
@@ -41,8 +41,20 @@ task.spawn(function()
 
             Player.Character.HumanoidRootPart.Anchored = true
             local Servers = ListServers()
-            local Server = Servers.data[math.random(1, #Servers.data)]
-            TPS:TeleportToPlaceInstance(_place, Server.id, Player)
+
+            local foundServer = nil
+            for _, server in pairs(Servers.data) do
+                local maxPlayers = server.maxPlayers
+                local playing = server.playing
+                if maxPlayers > (playing - 3) then
+                    foundServer = server
+                    break
+                end
+            end
+
+            if foundServer then
+                TPS:TeleportToPlaceInstance(_place, foundServer.id, Player)
+            end
             break
         end
     end
